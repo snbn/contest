83919320e33ec4cfa335803c498df7d301f6a99c
snbn <sb.as.engineer@gmail.com>
2020-01-03 23:27:13 +0900

    add new function `zAlgorithm`

---
+++ string.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> zAlgorithm(const char* s, int n) {
+  vector<int> z(n);
+  if (n == 0) {
+    return z;
+  }
+
+  int l = 0;
+  z[0] = 0;
+  for (int i = 1; i < n; i++) {
+    int r = l + z[l];
+
+    if (r > i + z[i - l]) {
+      z[i] = z[i - l];
+    } else {
+      z[i] = max(0, r - i);
+      l = i;
+      for (int j = z[i]; i + j < n && s[i + j] == s[j]; j++) {
+        z[i]++;
+      }
+    }
+  }
+  z[0] = n;
+
+  return move(z);
+}
