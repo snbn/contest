c78304299a332c500342eaf5e953a3591931b775
snbn <sb.as.engineer@gmail.com>
2020-05-20 16:31:12 +0900

    new class

---
+++ modint.cpp
@@ -0,0 +1,31 @@
+#include <cstdint>
+
+using std::int64_t;
+
+template <int64_t MOD>
+class Modint {
+  using Self = Modint<MOD>;
+
+  int64_t m_value;
+
+ public:
+  explicit Modint(int64_t value) : m_value(value % MOD) {}
+  Self& operator=(const Self& rh) {
+    m_value = rh.m_value;
+    return *this;
+  }
+  Self operator-() const { return Self(MOD - m_value); }
+  Self operator+(const Self& other) const {
+    return Self(m_value + other.m_value);
+  }
+  Self operator-(const Self& other) const {
+    return Self(m_value - other.m_value + MOD);
+  }
+  Self operator*(const Self& other) const {
+    return Self(m_value * other.m_value);
+  }
+  Self& operator+=(const Self& rh) { return (*this) = (*this) + rh; }
+  Self& operator-=(const Self& rh) { return (*this) = (*this) - rh; }
+  Self& operator*=(const Self& rh) { return (*this) = (*this) * rh; }
+  int64_t value() const { return m_value; }
+};
