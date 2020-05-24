85d829758f751daf4437b03534714c37122cdcf1
snbn <sb.as.engineer@gmail.com>
2020-05-24 21:54:53 +0900

    new feature

--- number_theory.cpp
+++ number_theory.cpp
@@ -1,3 +1,7 @@
+#include <utility>
+#include <vector>
+
+using namespace std;
 
 bool is_prime(int n) {
   for (int p = 2; p * p <= n; p++) {
@@ -7,4 +11,24 @@ bool is_prime(int n) {
   }
 
   return true;
+}
+
+vector<pair<int, int>> factorize(int n) {
+  vector<pair<int, int>> result;
+  for (int p = 2; p * p <= n; p++) {
+    if (is_prime(p)) {
+      int cnt = 0;
+      while (n % p == 0) {
+        n /= p;
+        cnt++;
+      }
+      if (cnt > 0) {
+        result.push_back(make_pair(p, cnt));
+      }
+    }
+  }
+  if (n != 1) {
+    result.push_back(make_pair(n, 1));
+  }
+  return move(result);
 }
