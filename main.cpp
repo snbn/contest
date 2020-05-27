3ce52a4b7f0c7d2f0d950927198d0ed51e5fa880
snbn <sb.as.engineer@gmail.com>
2020-05-27 14:49:23 +0900

    fix

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@
 
 using namespace std;
 
-#define rep(i, n) for (int64_t i = 0; i < (n); i++)
-#define irep(i, n) for (int64_t i = 0; i <= (n); i++)
+#define rep(i, n) for (int64_t i = 0; i < (int64_t)(n); i++)
+#define irep(i, n) for (int64_t i = 0; i <= (int64_t)(n); i++)
 #define rrep(i, n) for (int64_t i = (n)-1; i >= 0; i--)
 #define rirep(i, n) for (int64_t i = n; i >= 0; i--)
 
