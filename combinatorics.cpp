5a76dd9da757bbaf6de4ee11635d25fa57107039
snbn <sb.as.engineer@gmail.com>
2020-07-03 22:20:26 +0900

    for flexibility, `MOD` should be given to them as a parameter

--- combinatorics.cpp
+++ combinatorics.cpp
@@ -3,22 +3,18 @@
 
 using namespace std;
 
-const int64_t MOD = 1'000'000'007L;
-
-int64_t dpow(int64_t x, int64_t y) {
-  if (y == 0) {
+int64_t dpow(int64_t a, int64_t b, int64_t MOD) {
+  if (b == 0) {
     return 1;
   }
-  if (y % 2 == 0) {
-    return dpow(x * x % MOD, y / 2);
+  if (b % 2 == 0) {
+    return dpow(a * a % MOD, b / 2, MOD);
   } else {
-    return x * dpow(x, y - 1) % MOD;
+    return dpow(a, b - 1, MOD) * a % MOD;
   }
 }
 
-int64_t inv(int64_t x) { return dpow(x, MOD - 2); }
-
-int64_t comb(int64_t n, int64_t k) {
+int64_t comb(int64_t n, int64_t k, int64_t MOD) {
   int64_t a = 1, b = 1;
   for (int64_t i = 0; i < k; i++) {
     a *= (n - i) % MOD;
@@ -26,10 +22,10 @@ int64_t comb(int64_t n, int64_t k) {
     b *= (i + 1) % MOD;
     b %= MOD;
   }
-  return a * inv(b) % MOD;
+  return a * dpow(b, MOD - 2, MOD) % MOD;
 }
 
-int64_t perm(int64_t n, int64_t k) {
+int64_t perm(int64_t n, int64_t k, int64_t MOD) {
   int64_t r = 1;
   for (int64_t i = 0; i < k; i++) {
     r *= (n - i) % MOD;
