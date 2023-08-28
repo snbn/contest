ee5fe032851175fbb3685e443988b8e28789dd27
snbn <sb.as.engineer@gmail.com>
2023-08-28 18:29:09 +0900

    グローバルにusing namespaceしない

--- number_theory.cpp
+++ number_theory.cpp
@@ -1,9 +1,7 @@
-#include <sstream>
+#include <cassert>
 #include <tuple>
 #include <utility>
 #include <vector>
-
-using namespace std;
 
 bool is_prime(size_t n) {
   for (size_t p = 2; p * p <= n; p++) {
@@ -15,8 +13,8 @@ bool is_prime(size_t n) {
   return true;
 }
 
-vector<size_t> factors(size_t n) {
-  vector<size_t> result;
+std::vector<size_t> factors(size_t n) {
+  std::vector<size_t> result;
   for (size_t d = 1; d * d <= n; d++) {
     if (n % d == 0) {
       result.push_back(d);
@@ -28,8 +26,10 @@ vector<size_t> factors(size_t n) {
   return result;
 }
 
-vector<pair<size_t, size_t>> factorize(size_t n) {
-  vector<pair<size_t, size_t>> result;
+std::vector<std::pair<size_t, size_t>> factorize(size_t n) {
+  using std::make_pair;
+
+  std::vector<std::pair<size_t, size_t>> result;
   for (size_t p = 2; p * p <= n; p++) {
     if (is_prime(p)) {
       size_t cnt = 0;
@@ -45,11 +45,12 @@ vector<pair<size_t, size_t>> factorize(size_t n) {
   if (n != 1) {
     result.push_back(make_pair(n, 1));
   }
+
   return move(result);
 }
 
-vector<bool> eratosthenes_sieve(size_t size) {
-  vector<bool> is_prime(size + 1, true);
+std::vector<bool> eratosthenes_sieve(size_t size) {
+  std::vector<bool> is_prime(size + 1, true);
   for (size_t i = 0; i <= size; i++) {
     if (i < 2) {
       is_prime[i] = false;
@@ -63,10 +64,12 @@ vector<bool> eratosthenes_sieve(size_t size) {
 }
 
 size_t totient(const size_t n) {
-  vector<pair<size_t, size_t>> fs = factorize(n);
+  auto fs = factorize(n);
   size_t result = n;
 
   for (auto pa : fs) {
+    using std::tie, std::ignore;
+
     size_t p;
     tie(p, ignore) = pa;
     result /= p;
@@ -75,7 +78,8 @@ size_t totient(const size_t n) {
   return result;
 }
 
-tuple<int64_t, int64_t, int64_t> xgcd(int64_t a, int64_t b) {
+std::tuple<int64_t, int64_t, int64_t> xgcd(int64_t a, int64_t b) {
+  using std::tie, std::make_tuple;
   if (b == 0) {
     return make_tuple(1, 0, a);
   }
@@ -84,8 +88,10 @@ tuple<int64_t, int64_t, int64_t> xgcd(int64_t a, int64_t b) {
   return make_tuple(w, z - a / b * w, d);
 }
 
-tuple<int64_t, int64_t> chinese_rem(int64_t a1, int64_t a2, int64_t m1,
-                                    int64_t m2) {
+std::tuple<int64_t, int64_t> chinese_rem(int64_t a1, int64_t a2, int64_t m1,
+                                         int64_t m2) {
+  using std::tie, std::make_tuple;
+
   int64_t x, y, d;
   tie(x, y, d) = xgcd(m1, m2);
 
@@ -100,14 +106,11 @@ tuple<int64_t, int64_t> chinese_rem(int64_t a1, int64_t a2, int64_t m1,
   return make_tuple((sln % M + M) % M, M);
 }
 
-tuple<int64_t, int64_t> chinese_rem(const vector<int64_t>& a,
-                                    const vector<int64_t>& MOD) {
-  if (a.size() != MOD.size()) {
-    stringstream msg;
-    msg << "a.size() is " << a.size() << ", but MOD.size()" << MOD.size()
-        << "\n";
-    throw msg.str();
-  }
+std::tuple<int64_t, int64_t> chinese_rem(const std::vector<int64_t>& a,
+                                         const std::vector<int64_t>& MOD) {
+  assert(a.size() == MOD.size());
+
+  using std::tie, std::make_tuple;
   int64_t sln = 0, M = 1;
   for (size_t i = 0; i < a.size(); i++) {
     tie(sln, M) = chinese_rem(sln, a[i], M, MOD[i]);
