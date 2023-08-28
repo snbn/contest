81579af9f07a81ec15515bde03150ccec8552102
snbn <sb.as.engineer@gmail.com>
2023-08-28 20:09:32 +0900

    グローバルにusing namespaceしない

--- tree/euler_tour.cpp
+++ tree/euler_tour.cpp
@@ -1,20 +1,18 @@
 #include <vector>
 
-using namespace std;
-
 class EulerTour {
-  const vector<vector<int>> &mEdge;
+  const std::vector<std::vector<int>> &mEdge;
 
-  vector<int> mOrderL, mOrderR, mSorted;
+  std::vector<int> mOrderL, mOrderR, mSorted;
 
  public:
-  explicit EulerTour(const vector<vector<int>> &edge)
+  explicit EulerTour(const std::vector<std::vector<int>> &edge)
       : mEdge(edge), mOrderL(edge.size()), mOrderR(edge.size()) {
     dfs(0, -1);
   }
   int l(int node) const { return mOrderL[node]; }
   int r(int node) const { return mOrderR[node]; }
-  const vector<int> &tour() const { return mSorted; }
+  const std::vector<int> &tour() const { return mSorted; }
 
  private:
   void dfs(int node, int parent) {
